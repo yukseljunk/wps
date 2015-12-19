3b6540afcd248364ed2fb14c735f1d6309a1ea6b
Yuksel Daskin <daskiny@rferl.org>
2015-12-20 00:12:12 +0100

    started dealing with thumnbnail problems

--- src/WordPressSharp/Models/ImagePost.cs
+++ src/WordPressSharp/Models/ImagePost.cs
@@ -19,6 +19,11 @@ namespace WordPressSharp.Models
         }
 
         public string Alt { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int ThumbnailWidth { get; set; }
+        public int ThumbnailHeight { get; set; }
 
     }
 }
