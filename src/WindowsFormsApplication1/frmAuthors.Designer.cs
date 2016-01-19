17378e07bfbf42062dc3ba0f716dca437edf96cc
Yuksel Daskin <daskiny@rferl.org>
2016-01-19 23:54:25 +0100

    authors form

---
+++ src/WindowsFormsApplication1/frmAuthors.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WordpressScraper
+{
+    partial class frmAuthors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtAuthors = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreate.Location = new System.Drawing.Point(328, 383);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(410, 382);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtAuthors
+            // 
+            this.txtAuthors.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAuthors.Location = new System.Drawing.Point(12, 12);
+            this.txtAuthors.Multiline = true;
+            this.txtAuthors.Name = "txtAuthors";
+            this.txtAuthors.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAuthors.Size = new System.Drawing.Size(473, 352);
+            this.txtAuthors.TabIndex = 2;
+            // 
+            // frmAuthors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(507, 417);
+            this.Controls.Add(this.txtAuthors);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "frmAuthors";
+            this.Text = "frmAuthors";
+            this.Load += new System.EventHandler(this.frmAuthors_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtAuthors;
+    }
+}
---
+++ src/WindowsFormsApplication1/frmAuthors.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+using PttLib;
+using WordpressScraper.Dal;
+
+namespace WordpressScraper
+{
+    public partial class frmAuthors : Form
+    {
+        private ProgramOptions _options;
+
+        public frmAuthors()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAuthors_Load(object sender, EventArgs e)
+        {
+            var optionsFactory = new ProgramOptionsFactory();
+            _options = optionsFactory.Get();
+        }
+
+        private string MySqlConnectionString
+        {
+            get
+            {
+                return string.Format("Server={0};Database={1};Uid={2};Pwd={3}; Allow User Variables=True", _options.DatabaseUrl, _options.DatabaseName, _options.DatabaseUser, _options.DatabasePassword);
+            }
+        }
+
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAuthors.Text))
+            {
+                return;
+            }
+            var authors = txtAuthors.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            using (var dal = new Dal.Dal(MySqlConnectionString))
+            {
+                var userDal = new UserDal(dal);
+                foreach (var author in authors)
+                {
+                    if (string.IsNullOrEmpty(author.Trim()))
+                    {
+                        continue;
+                    }
+                    userDal.InsertUser(author.Trim(),_options.BlogHost);
+                }
+            }
+            MessageBox.Show("Done");
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Close();
+                this.Dispose();
+            }
+            catch
+            {
+            }
+
+        }
+    }
+}
