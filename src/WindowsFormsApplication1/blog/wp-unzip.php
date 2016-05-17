a1b3740ec0d85f697476437e18260c5d88e3cef3
Yuksel Daskin <daskiny@rferl.org>
2016-05-17 23:05:31 +0200

    wsod

--- src/WindowsFormsApplication1/blog/wp-unzip.php
+++ src/WindowsFormsApplication1/blog/wp-unzip.php
@@ -1,13 +1,19 @@
 <?php
+error_reporting( E_ALL );
+ini_set('display_startup_errors',1); 
+ini_set("display_errors", 1);
 $file=htmlspecialchars($_GET["file"]);
-$zip = new ZipArchive;
-$res = $zip->open($file);
-if ($res === TRUE) {
-  $zip->extractTo('wp-content/plugins/');
-  $zip->close();
-  echo 'OK';
-} else {
-  echo 'Problem';
-}
-?>
 
+  $zip = new ZipArchive;
+  $res = $zip->open($file);
+  if ($res === TRUE) {
+    $zip->extractTo('wp-content/plugins/');
+    $zip->close();
+    echo 'OK';
+  } else {
+    echo 'Problem';
+  }
+ 
+
+echo error_get_last() 
+?>
